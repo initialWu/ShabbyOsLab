07446d2e75e216868c64f2b79e2c63d401760625
olleh-dlrow <1136246408@qq.com>
2021-12-12 18:51:50 +0800

    convert CRLF to LF

---
+++ command/allocfree.c
@@ -0,0 +1,16 @@
+# include "stdio.h"
+# include "type.h"
+
+int main(int argc, char * argv[])
+{
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("OP INPUT:\n");
+    printf("ALLOC: 1\n");
+    printf("FREE: 0\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+    
+
+    return 0;
+}
+
